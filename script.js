bc7e33d7f67311eed6f15dd8e9be43cad43112ed
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-05-18 01:48:37 -0400

    Async function

--- script.js
+++ script.js
@@ -2,10 +2,26 @@
 
 const breed = document.getElementById("breedSelect")
 
-fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+fetch("https://api.thedogapi.com/v1/images/search?limit=10")
 .then((res) => {
     return res.json();
 })
 .then((data) => {
     console.log(data)
 });
+
+async function initialLoad() {
+    try {
+      const response = await fetch('https://api.thedogapi.com/v1/breeds');
+      const breedData = await response.json();
+  
+      const breedSelect = document.getElementById('breedSelect');
+
+breed.forEach(breed => {
+    const option = document.createElement('option');
+    option.value = breed.id;
+    option.textContent = breed.name;
+    dropdown.appendChild(option);
+});
+}
+    }
