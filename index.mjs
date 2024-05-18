a2de9b28d82a00cf2fc1169a9968c435b0afd601
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-05-17 23:24:00 -0400

    fetched data

--- index.mjs
+++ index.mjs
@@ -1,3 +1,5 @@
-fetch (" https://api.thedogapi.com/v1/breeds");
-.then(response = console.log(response))
-.catch(error => console.error(error))
+
+    
+    
+    
+    
--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("SBA 4")
+console.log("Is this working?");
+
+const breed = document.getElementById("breedSelect")
+
+fetch("https://api.thedogapi.com/v1/images/search?limit=10")
+.then((res) => {
+    return res.json();
+})
+.then((data) => {
+    console.log(data)
+})
