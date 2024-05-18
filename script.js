473b90aaf4971baef52ee29d3dc1f5cd55bdc0b1
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-05-18 01:06:09 -0400

    fetched data

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-console.log("Is this working?");
+/// console.log("Is this working?");
 
 const breed = document.getElementById("breedSelect")
 
-fetch("https://api.thedogapi.com/v1/images/search?limit=10")
+fetch("https://api.thecatapi.com/v1/images/search?limit=10")
 .then((res) => {
     return res.json();
 })
 .then((data) => {
     console.log(data)
-})
+});
